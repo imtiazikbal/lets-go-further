4600f083609179de862d935c83dfbf18012166df
imtiazikbal <imtiaz.ikbal.com@gmail.com>
2024-12-17 00:47:39 +0600

    go

---
+++ cmd/api/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	// Declare an instance of the config struct.
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(log.Writer(), "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a HTTP server with some sensible timeout settings, which listens on the
+	// port provided in the config struct and uses the servemux we created above as the
+	// handler.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Start the HTTP server.
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
---
+++ cmd/api/movies.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"greenlight/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier). This struct will be our *target
+	// decode destination*.
+	var input struct {
+		ID        int64     `json:"id"`
+		CreatedAt time.Time `json:"-"`
+		Title     string    `json:"title"`
+		Year      int32     `json:"year,omitempty"`
+		Runtime   int32     `json:"runtime"`
+		Genres    []string  `json:"genres"`
+		Version   int32     `json:"version"`
+	}
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	// Importantly, notice that when we call Decode() we pass a *pointer* to the input
+	// struct as the target decode destination. If there was an error during decoding,
+	// we also use our generic errorResponse() helper to send the client a 400 Bad
+	// Request response containing the error message.
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	// Dump the contents of the input struct in a HTTP response.
+	app.writeJSON(w, http.StatusCreated, envelope{"movie": input}, nil)
+
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		Title:     "The Dark Knight",
+		Version:   1,
+		Runtime:   152,
+		Genres:    []string{"Action", "Crime", "Drama"},
+		CreatedAt: time.Now(),
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
